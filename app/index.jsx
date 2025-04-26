3df2f42139237da017cb96dc96ce652b8e6967e2
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-26 11:21:23 +0100

    use Text, View and Image component

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,45 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+
+import Logo from '../assets/img/logo_light.png';
 
 const Home = () => {
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={styles.container}>
+            <Image source={Logo} style={styles.img} />
+
+            <Text style={styles.title}>The Number 1</Text>
+
+            <Text style={{marginTop: 10, marginBottom: 30}}>
+                Reading List App
+            </Text>
+
+            <View style={styles.card}>
+                <Text>Hello, this is a Card</Text>
+            </View>
         </View>
     );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#e0dfe8',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    img: {
+        marginVertical: 20,
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+    card: {
+        backgroundColor: '#fff',
+        padding: 20,
+        borderRadius: 5,
+        boxShadow: '4px 4px rgba(0,0,0,0.1)',
+    },
+});
