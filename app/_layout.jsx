1eb9594a45f0f6937f22ff87b44d064115e5a403
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-26 11:47:20 +0100

    add light and dark scheme capabilities

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,19 +1,28 @@
 import {Stack} from 'expo-router';
+import {useColorScheme} from 'react-native';
+import {Colors} from '../constants/Colors';
+import {StatusBar} from 'expo-status-bar';
 
 export default function RootLayout() {
+    const colorScheme = useColorScheme();
+    const theme = Colors[colorScheme] ?? Colors.light;
+
     return (
-        <Stack
-            screenOptions={{
-                headerStyle: {backgroundColor: '#ddd'},
-                headerTintColor: '#333',
-            }}>
-            {/* Individual Screens */}
-            <Stack.Screen name="index" options={{title: 'Home'}} />
-            <Stack.Screen name="about" options={{title: 'About'}} />
-            <Stack.Screen
-                name="contact"
-                options={{title: 'Contact', headerShown: false}}
-            />
-        </Stack>
+        <>
+            <StatusBar value="auto" />
+            <Stack
+                screenOptions={{
+                    headerStyle: {backgroundColor: theme.navBackground},
+                    headerTintColor: theme.title,
+                }}>
+                {/* Individual Screens */}
+                <Stack.Screen name="index" options={{title: 'Home'}} />
+                <Stack.Screen name="about" options={{title: 'About'}} />
+                <Stack.Screen
+                    name="contact"
+                    options={{title: 'Contact', headerShown: false}}
+                />
+            </Stack>
+        </>
     );
 }
