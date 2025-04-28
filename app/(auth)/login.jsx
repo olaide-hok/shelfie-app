732e1779687a1a43eacd8966eae9e800830fd1ec
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-04-28 16:55:17 +0100

    useUser hook

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -6,6 +6,7 @@ import {
 } from 'react-native';
 import {Link} from 'expo-router';
 import {useState} from 'react';
+import {useUser} from '../../hooks/useUser';
 
 import ThemedView from '../../components/ThemedView';
 import ThemedText from '../../components/ThemedText';
@@ -17,7 +18,10 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const {user} = useUser();
+
     const handleSubmit = async () => {
+        console.log('current user value: ', user);
         console.log('login form submitted: ', email, password);
     };
 
