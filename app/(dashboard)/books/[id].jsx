5e4bc7c8b516d2fa162e9204d6aa945e7d64926e
Habeeb Kareem <olaidekareem43@yahoo.com>
2025-05-28 15:42:17 +0100

    render dynamic book details via id

--- app/(dashboard)/books/[id].jsx
+++ app/(dashboard)/books/[id].jsx
@@ -1,5 +1,7 @@
 import {StyleSheet} from 'react-native';
 import {useLocalSearchParams} from 'expo-router';
+import {useEffect, useState} from 'react';
+import {useBooks} from '../../../hooks/useBooks';
 
 // themed components
 import ThemedText from '../../../components/ThemedText';
@@ -7,13 +9,43 @@ import ThemedButton from '../../../components/ThemedButton';
 import ThemedView from '../../../components/ThemedView';
 import Spacer from '../../../components/Spacer';
 import ThemedCard from '../../../components/ThemedCard';
+import ThemedLoader from '../../../components/ThemedCard';
 
 const BookDetails = () => {
+    const [book, setBook] = useState(null);
+
     const {id} = useLocalSearchParams();
+    const {fetchBookById} = useBooks();
+
+    useEffect(() => {
+        async function loadBook() {
+            const bookData = await fetchBookById(id);
+            setBook(bookData);
+        }
+
+        loadBook();
+    }, [id]);
+
+    if (!book) {
+        return (
+            <ThemedView safe={true} style={styles.container}>
+                <ThemedLoader />
+            </ThemedView>
+        );
+    }
 
     return (
         <ThemedView safe={true} style={styles.container}>
-            <ThemedText>{id}</ThemedText>
+            <ThemedCard style={styles.card}>
+                <ThemedText style={styles.title}>{book.title}</ThemedText>
+                <ThemedText>Written by {book.author}</ThemedText>
+                <Spacer />
+
+                <ThemedText title={true}>Book description:</ThemedText>
+                <Spacer height={10} />
+
+                <ThemedText>{book.description}</ThemedText>
+            </ThemedCard>
         </ThemedView>
     );
 };
@@ -24,5 +56,12 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'stretch',
+    },
+    title: {
+        fontSize: 22,
+        marginVertical: 10,
+    },
+    card: {
+        margin: 20,
     },
 });
